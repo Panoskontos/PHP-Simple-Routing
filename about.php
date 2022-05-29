0aa102a2b5289f596f02e4e72f8b87e508a93544
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-05-29 19:21:23 +0300

    f

---
+++ about.php
@@ -0,0 +1,47 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+
+    <style>
+        .container{
+            max-width: 1200px;
+            margin: 0 auto;
+        }
+
+        header{
+            margin:100px;           
+        }
+
+        header a{
+            display: inline-block;
+            padding: 20px;
+        }
+    </style>
+
+</head>
+
+<body>
+    <div class="container">
+    <?php
+        include('header.php')
+    ?>  
+
+        <section>
+            <h1>About</h1>
+
+        </section>
+    </div>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,10 @@
+<!-- We can create files that we will use in every page -->
+
+<header>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="projects.php">Projects</a>
+            <a href="contact.php">Contact</a>
+    </nav>
+</header>
